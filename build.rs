5e5cf24daf22ed46374f6df5487e3b8060022b8b
Jonathan Kingston <jonathan@jooped.co.uk>
2018-09-10 20:48:15 +0100

    Add in codegen module, moving code from parser

--- build.rs
+++ build.rs
@@ -1,26 +1,19 @@
-extern crate parser;
+extern crate codegen;
 
 use std::env;
 use std::fs::File;
 use std::io::Write;
 use std::path::Path;
 
-use parser::Interfaces;
+use codegen::Codegen;
 extern crate failure;
 use failure::ResultExt;
 
 fn main() -> Result<(), failure::Error> {
     let out_dir = env::var("OUT_DIR").context("reading OUT_DIR environment variable")?;
-    let interfaces = Interfaces::parse().unwrap();
+    let codegen = Codegen::gen();
     let dest_path = Path::new(&out_dir).join("bindings.rs");
     let mut f = File::create(&dest_path).unwrap();
-
-    // Code generating this macro because I can't find a way to make generics to line up for passing in an optional attribute holder whilst then calling methods on it in the containing structure
-    write!(f, "{}", interfaces.get_macro());
-
-    write!(f, "{}", "pub mod attr { pub use Attributish;");
-    write!(f, "{}", interfaces.get_interfaces_code());
-    write!(f, "{}", interfaces.get_interface_code("HTMLElement"));
-    write!(f, "{}", "}");
+    write!(f, "{}", codegen.get_code());
     Ok(())
 }
--- crates/parser/src/lib.rs
+++ crates/parser/src/lib.rs
@@ -10,12 +10,6 @@ use sourcefile::SourceFile;
 use std::ffi::OsStr;
 use std::fs;
 
-extern crate proc_macro2;
-#[macro_use]
-extern crate quote;
-use proc_macro2::{Ident, Span, TokenStream};
-use quote::ToTokens;
-
 extern crate heck;
 use heck::SnakeCase;
 
@@ -26,7 +20,7 @@ pub struct Interfaces {
 }
 
 impl Interfaces {
-    fn tag_interfaces<'a>(&self) -> HashMap<&'a str, &'a str> {
+    pub fn tag_interfaces<'a>(&self) -> HashMap<&'a str, &'a str> {
         let mut interfaces = HashMap::new();
         interfaces.insert("div", "HTMLDivElement");
         interfaces.insert("label", "HTMLLabelElement");
@@ -168,157 +162,8 @@ HTMLTableCaptionElement
         return false;
     }
 
-    fn method_calls(&self, interface_name: &str, el: Ident) -> Vec<proc_macro2::TokenStream> {
-        let mut interface_calls = vec![];
-        if let Some(methods) = self.data.get(interface_name) {
-            for field in methods {
-                let field_ident = Ident::new(field, Span::call_site());
-                let attr_name = field.to_string();
-                let set_name = "set_".to_string() + &attr_name;
-                let setter = Ident::new(&set_name, Span::call_site());
-                interface_calls.push(quote!{
-                    if let Some(ref field) = self.#field_ident {
-                       #el.#setter(&field.clone());
-                    }
-                });
-            }
-        }
-        println!("iface {:?}", interface_calls);
-        interface_calls
+    pub fn get_methods(&self, interface_name: &str) -> Option<&Vec<String>> {
+      self.data.get(interface_name)
     }
 
-    pub fn get_macro(&self) -> String {
-        let mut tokens = TokenStream::new();
-
-        let mut arms = vec![];
-        for (tag, interface) in &self.tag_interfaces() {
-            let tag_string = String::from(tag.clone());
-            let tag_ident = Ident::new(&tag_string, Span::call_site());
-            let interface_name = Ident::new(&format!("{}Attributes", interface), Span::call_site());
-            println!("{}, {}", tag_string, interface_name);
-            arms.push(quote!{
-               (#tag_string, {$( $key:ident : $value:expr ),*}) => {
-                   {
-                       let attrs = #interface_name {
-                           $( $key: Some($value.into()), )*
-                           ..Default::default()
-                       };
-                       let el_container = El {
-                           el: None,
-                           name: #tag_string.to_string(),
-                           attrs
-                       };
-                       el_container
-                   }
-               };
-            });
-        }
-
-        (quote!{
-            #[macro_export]
-            macro_rules! create_element {
-                #(#arms)*
-                ($name:tt, {$( $key:ident : $value:expr ),*}) => {
-                    {
-                        let attrs = HTMLElementAttributes {
-                            $( $key: Some($value.into()), )*
-                            ..Default::default()
-                        };
-                        let el_container = El {
-                            el: None,
-                            name: $name.into(),
-                            attrs
-                        };
-                        el_container
-                    }
-                }
-            }
-        }).to_tokens(&mut tokens);
-
-        tokens.to_string()
-    }
-
-    pub fn get_interfaces_code(&self) -> String {
-        let mut interfaces = String::new();
-        for (tag, interface) in &self.tag_interfaces() {
-            interfaces += &self.get_interface_code(interface);
-        }
-        interfaces
-    }
-
-    pub fn get_interface_name(&self, interface_name: &str) -> String {
-        String::from(interface_name).replace("HTML", "Html")
-    }
-
-    //TODO neaten
-    pub fn get_interface_code(&self, interface_name: &str) -> String {
-        let mut tokens = TokenStream::new();
-        let mut fields = vec![];
-        let mut other_interface = quote!{};
-        let interface_calls =
-            self.method_calls(interface_name, Ident::new("html_el", Span::call_site()));
-        let main_interface_calls =
-            self.method_calls("Element", Ident::new("el", Span::call_site()));
-
-        if let Some(methods) = self.data.get(interface_name) {
-            for field in methods {
-                let field_ident = Ident::new(field, Span::call_site());
-                fields.push(quote!{pub #field_ident: Option<String>,});
-            }
-        }
-        if interface_name != "HTMLElement" {
-            let other_interface_calls =
-                self.method_calls("HTMLElement", Ident::new("html_el", Span::call_site()));
-            other_interface = quote!{
-              let dyn_el: Option<&web_sys::HtmlElement> = wasm_bindgen::JsCast::dyn_ref(&el);
-              dyn_el.map(|html_el| {
-                  #(#other_interface_calls)*
-              });
-            };
-            if let Some(methods) = self.data.get("HTMLElement") {
-                for field in methods {
-                    let field_ident = Ident::new(field, Span::call_site());
-                    fields.push(quote!{pub #field_ident: Option<String>,});
-                }
-            }
-        }
-        if let Some(methods) = self.data.get("Element") {
-            for field in methods {
-                let field_ident = Ident::new(field, Span::call_site());
-                fields.push(quote!{pub #field_ident: Option<String>,});
-            }
-        }
-        let web_sys_ident = Ident::new(&self.get_interface_name(interface_name), Span::call_site());
-
-        let interface = quote!{
-          let dyn_el: Option<&web_sys::#web_sys_ident> = wasm_bindgen::JsCast::dyn_ref(&el);
-          dyn_el.map(|html_el| {
-              #(#interface_calls)*
-          });
-        };
-        let interface_name = format!("{}Attributes", interface_name);
-        let interface_ident = Ident::new(&interface_name, Span::call_site());
-
-        (quote!{
-          #[derive(Default)]
-          pub struct #interface_ident {
-              #(#fields)*
-          }
-          impl Attributish for #interface_ident {
-              fn flush(&self, el: web_sys::Element) -> web_sys::Element {
-                // TODO this inheritance tree should be defined from the parsed webidl
-                {
-                  #interface
-                }
-                {
-                  #other_interface
-                }
-                #(#main_interface_calls)*
-                el
-              }
-          }
-        }).to_tokens(&mut tokens);
-
-        tokens.to_string()
-    }
 }
